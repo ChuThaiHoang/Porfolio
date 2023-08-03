f0dcc096dcec1d256d4099b5f92e76f548dba002
Chu Thái Hoàng <hoangchuzz2003@gmail.com>
2023-08-03 15:56:45 +0700

    update style switcher(end)

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -27,8 +27,13 @@ function setActiveStyle(color)
 }
 //Light mode and Dark mode
 const DayNight = document.querySelector(".day-night");
+DayNight.addEventListener("click", () => {
+    DayNight.querySelector("i").classList.toggle("fa-sun");
+    DayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
 window.addEventListener("load", () => {
-    if (document.body.classList.contains("dark"))
+    if (document.body.classList.contains(".dark"))
     {
         DayNight.querySelector("i").classList.add("fa-sun");
     }
