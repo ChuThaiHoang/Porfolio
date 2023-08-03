480bdc3c8aeb4146b83e09b88bd577dc410675d3
Chu Thái Hoàng <hoangchuzz2003@gmail.com>
2023-08-03 16:18:30 +0700

    update Scrip.js(Typing Animation)

---
+++ js/Script.js
@@ -0,0 +1,7 @@
+// Text typing Animation
+var typed = new Typed(".typing", {
+    strings:["Software engineer", "Java Developer", "Android Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
