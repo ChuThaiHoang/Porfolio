9ee87582d57da4daf8c47fa2e3d2f906bf5bc1e0
Chu Thái Hoàng <hoangchuzz2003@gmail.com>
2023-08-03 16:22:07 +0700

    update

--- js/Script.js
+++ js/Script.js
@@ -1,6 +1,6 @@
 // Text typing Animation
 var typed = new Typed(".typing", {
-    strings:["Software engineer", "Java Developer", "Android Developer"],
+    strings:["", "Software engineer", "Java Developer", "Android Developer"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
